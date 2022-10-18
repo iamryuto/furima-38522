f393fbcffc29e3668b71cdabd8521fd0b7d346f5
iamryuto <oeryuto@icloud.com>
2022-10-18 17:38:01 +0900

    active hashの記載

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,25 @@ class Item < ApplicationRecord
   validates :item_name, presence: true
   validates :explanation, presence: true
   validates :price, presence: true
+  validates :image, presence: true
 
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :state
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :fee
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :day
+
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :state_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :fee_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :day_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
--- db/migrate/20221018061519_create_items.rb
+++ db/migrate/20221018061519_create_items.rb
@@ -4,7 +4,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.string :item_name, null: false
       t.text :explanation, null: false
       t.integer :price, null: false
-      t.integer :days_id, null: false
+      t.integer :day_id, null: false
       t.integer :prefecture_id, null: false
       t.integer :category_id, null: false
       t.integer :state_id, null: false
