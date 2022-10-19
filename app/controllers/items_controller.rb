9acedf05bc7320d121d8246be1e34fb5ed4a9b94
iamryuto <oeryuto@icloud.com>
2022-10-19 16:28:41 +0900

    Revert "routing, controllerの定義"
    This reverts commit 2ec29f8559c41de7fc5d550a17fe1e3f9fee0a5e.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
-  before_action :move_to_index, only: [:edit]
+  before_action :authenticate_user!, only: [:new]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -23,29 +22,10 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  def edit
-    @item = Item.find(params[:id])
-  end
-
-  def update
-    @item = Item.find(params[:id])
-    if @item.update(item_params)
-      redirect_to item_path(@item)
-    else
-      render :edit
-    end
-  end
-
   private
+
   def item_params
     params.require(:item).permit(:item_name, :image, :explanation, :price, :category_id, :state_id, :fee_id, :prefecture_id,
                                  :day_id).merge(user_id: current_user.id)
-  end
-
-  def move_to_index
-    @item = Item.find(params[:id])
-    unless user_signed_in? && current_user.id == @item.user_id
-      redirect_to root_path
-    end
   end
 end
