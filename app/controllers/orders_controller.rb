d2271c00995214348f11c75b4f99541c14e333f2
iamryuto <oeryuto@icloud.com>
2022-10-21 12:54:10 +0900

    商品購入機能

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,16 +8,24 @@ class OrdersController < ApplicationController
 
   def create
     @order_address = OrderAddress.new(order_params)
-    if @order_address.save
+    if @order_address.valid?
+      item = Item.find(params[:item_id])
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: item.price,  
+        card: order_params[:token],    
+        currency: 'jpy'                 
+      )
+      @order_address.save
       redirect_to root_path
     else
-      render :new
+      @item = Item.find(params[:item_id])
+      render :index
     end
   end
 
   private
-
   def order_params
-    params.require(:order_address).permit(:zip, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], order_id: params[:order_id])
+    params.require(:order_address).permit(:zip, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -10,12 +10,11 @@ class OrderAddress
     validates :city
     validates :address
     validates :phone_number, format: {with: /\A[0-9]{10,11}+\z/, message: "is invalid. Input only number"}
-    validates :order_id
   end
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    Address.create(zip: zip, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, order_id: order_id)
+    Address.create(zip: zip, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, order_id: order.id)
   end
 end
